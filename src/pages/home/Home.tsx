d47e3adff0ce518b69762577c70601a4a65da544
viviane santos <vjsantos.viviane@gmail.com>
2025-09-04 06:54:50 -0300

    refactor: apply semantic tag

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,7 +2,7 @@
 function Home() {
     return (
         <>
-            <div className="bg-indigo-900 flex justify-center">
+            <main className="bg-indigo-900 flex justify-center">
                 <div className='container grid grid-cols-2 text-white'>
                     <div className="flex flex-col gap-4 items-center justify-center py-4">
                         <h2 className='text-5xl font-bold'>
@@ -29,7 +29,7 @@ function Home() {
                         />
                     </div>
                 </div>
-            </div>
+            </main>
         </>
     )
 }
