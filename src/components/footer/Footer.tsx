f80d3d591591f59b6b7279dd649bb5a4e83485ef
viviane santos <vjsantos.viviane@gmail.com>
2025-09-08 19:09:17 -0300

    chore: modify logic navbar and footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,45 +1,53 @@
 import {
-  FacebookLogoIcon,
+  GithubLogoIcon,
   InstagramLogoIcon,
   LinkedinLogoIcon,
 } from "@phosphor-icons/react";
+import { useContext, type ReactNode } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
 
 function Footer() {
-  let data = new Date().getFullYear();
+  const data = new Date().getFullYear();
 
-  return (
-    <>
-      <footer className='flex justify-center text-white bg-indigo-900'>
-        <div className='container flex flex-col items-center py-4'>
-          <p className='text-xl font-bold'>
+  const { user } = useContext(AuthContext);
+
+  let component: ReactNode;
+
+  if (user.token !== "") {
+    component = (
+      <footer className="flex justify-center text-white bg-indigo-900">
+        <div className="container flex flex-col items-center py-4">
+          <p className="text-xl font-bold">
             Blog Pessoal Generation | Copyright: {data}
           </p>
 
-          <p className="text-lg">
-            Acesse nossas redes sociais
-          </p>
+          <p className="text-lg">Acesse nossas redes sociais</p>
 
-          <ul className='flex gap-2'>
+          <ul className="flex gap-2">
             <li>
-              <a href='https://www.linkedin.com/school/generationbrasil' target='_blank'>
-                <LinkedinLogoIcon size={48} weight='bold' />
+              <a href="https://www.linkedin.com/in/vsantosj" target="_blank">
+                <LinkedinLogoIcon size={48} weight="bold" />
               </a>
             </li>
             <li>
-              <a href='https://www.instagram.com/generationbrasil' target='_blank'>
-                <InstagramLogoIcon size={48} weight='bold' />
+              <a
+                href="https://www.instagram.com/vivi_santos_dev"
+                target="_blank"
+              >
+                <InstagramLogoIcon size={48} weight="bold" />
               </a>
             </li>
             <li>
-              <a href='https://www.facebook.com/generationbrasil' target='_blank'>
-                <FacebookLogoIcon size={48} weight='bold' />
+              <a href="https://github.com/vsantosj" target="_blank">
+                <GithubLogoIcon size={48} weight="bold" />
               </a>
             </li>
           </ul>
         </div>
       </footer>
-    </>
-  );
+    );
+  }
+  return <>{component}</>;
 }
 
 export default Footer;
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,45 +1,60 @@
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthContext";
-import { useContext } from "react";
-
+import { useContext, type ReactNode } from "react";
 
 function Navbar() {
-
   const navigate = useNavigate();
 
-  const { handleLogout } = useContext(AuthContext);
+  const { user, handleLogout } = useContext(AuthContext);
 
   function logout() {
     handleLogout();
-    alert('O Usuario foi deslogado com sucesso!');
-    navigate('/');
+    alert("O Usuario foi deslogado com sucesso!");
+    navigate("/");
   }
+  let component: ReactNode;
 
-  return (
-    <>
-      <header className='w-full bg-indigo-900 text-white flex justify-center py-4'>
-        <nav className='container flex justify-between text-lg'>
-          <Link to='/' className="text-2xl font-bold">Blog Pessoal</Link>
+  if (user.token !== "") {
+    component = (
+      <header className="w-full flex justify-center py-4 bg-indigo-900 text-white">
+        <nav className="container flex justify-between text-lg mx-8">
+          <Link to="/home" className="text-2xl font-bold">
+            Blog Pessoal
+          </Link>
 
-          <ul className='flex gap-4'>
+          <ul className="flex gap-4">
             <li>
-              <Link to='/posts' className='hover:underline'>Postagens</Link>
+              <Link to="/posts" className="hover:underline">
+                Postagens
+              </Link>
             </li>
             <li>
-              <Link to='/temas' className='hover:underline'>Temas</Link>
+              <Link to="/temas" className="hover:underline">
+                Temas
+              </Link>
             </li>
             <li>
-              <Link to='/cadastrartema' className='hover:underline'>Cadastrar tema</Link>
+              <Link to="/cadastrartema" className="hover:underline">
+                Cadastrar tema
+              </Link>
             </li>
-            <li><Link to='/perfil' className='hover:underline'>Perfil</Link></li>
             <li>
-              <Link to="/" onClick={logout}>Sair</Link>
+              <Link to="/perfil" className="hover:underline">
+                Perfil
+              </Link>
+            </li>
+            <li>
+              <Link to="/" onClick={logout}>
+                Sair
+              </Link>
             </li>
           </ul>
         </nav>
       </header>
-    </>
-  );
+    );
+  }
+
+  return <>{component}</>;
 }
 
 export default Navbar;
